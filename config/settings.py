b0bc9f94269d354f971cab1083a27697cd2c2daf
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-08-07 11:20:43 +0500

    add cors middleware

--- config/settings.py
+++ config/settings.py
@@ -92,6 +92,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.locale.LocaleMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
